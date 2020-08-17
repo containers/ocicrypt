07d99cd5b0691aed3522f5fdf80b194f39861b93
Stefan Berger <stefanb@linux.vnet.ibm.com>
2020-08-17 10:01:04 -0400

    pkcs11: Add high level keywrapping support for pkcs11
    For encryption we expect public keys in Parameters["pkcs11-pubkeys"]
    (PEM format) or pkcs11 YAML files may be in Parameters["pkcs11-yamls"].
    
    For decryption we expect pkcs11 YAML files that represent private keys
    in Parameters["pkcs11-yamls"].
    
    The Pkcs11Config object needs to go into the DecryptConfig's
    Parameters["pkcs11-config"]. It is needed by pkcs11 for encryption and
    decryption to find the location of the pkcs11 modules.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ keywrap/pkcs11/keywrapper_pkcs11_test.go
@@ -0,0 +1,214 @@
+/*
+   Copyright The ocicrypt Authors.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package pkcs11
+
+import (
+	"os"
+	"testing"
+
+	"github.com/containers/ocicrypt/config"
+	"github.com/containers/ocicrypt/utils"
+	"github.com/containers/ocicrypt/utils/softhsm"
+)
+
+var (
+	SOFTHSM_SETUP = "../../scripts/softhsm_setup"
+)
+
+func getPkcs11ConfigYaml(t *testing.T) []byte {
+	// we need to provide a configuration file so that on the various distros
+	// the libsofthsm2.so will be found by searching directories
+	config := `module-directories:
+ - /usr/lib64/pkcs11/  # Fedora
+ - /usr/lib/softhsm/   # Ubuntu
+`
+	return []byte(config)
+}
+
+func createValidPkcs11Ccs(t *testing.T) ([]*config.CryptoConfig, *softhsm.SoftHSMSetup, error) {
+	shsm := softhsm.NewSoftHSMSetup()
+	pkcs11PubKeyUriStr, err := shsm.RunSoftHSMSetup(SOFTHSM_SETUP)
+	if err != nil {
+		return nil, shsm, err
+	}
+	pubKeyPem, err := shsm.RunSoftHSMGetPubkey(SOFTHSM_SETUP)
+	if err != nil {
+		return nil, shsm, err
+	}
+	pkcs11PrivKeyYaml := `
+pkcs11:
+  uri: ` + pkcs11PubKeyUriStr + `
+module:
+  env:
+    SOFTHSM2_CONF: ` + shsm.GetConfigFilename()
+
+	p11confYaml := getPkcs11ConfigYaml(t)
+
+	validPkcs11Ccs := []*config.CryptoConfig{
+		// Key 1
+		{
+			EncryptConfig: &config.EncryptConfig{
+				Parameters: map[string][][]byte{
+					"pkcs11-pubkeys": {[]byte(pubKeyPem)},
+				},
+				DecryptConfig: config.DecryptConfig{
+					Parameters: map[string][][]byte{
+						"pkcs11-yamls":  {[]byte(pkcs11PrivKeyYaml)},
+						"pkcs11-config": {p11confYaml},
+					},
+				},
+			},
+
+			DecryptConfig: &config.DecryptConfig{
+				Parameters: map[string][][]byte{
+					"pkcs11-yamls":  {[]byte(pkcs11PrivKeyYaml)},
+					"pkcs11-config": {p11confYaml},
+				},
+			},
+		},
+		// Key 2
+		{
+			EncryptConfig: &config.EncryptConfig{
+				Parameters: map[string][][]byte{
+					// public and private key YAMLs are identical
+					"pkcs11-yamls": {[]byte(pkcs11PrivKeyYaml)},
+				},
+				DecryptConfig: config.DecryptConfig{
+					Parameters: map[string][][]byte{
+						"pkcs11-yamls":  {[]byte(pkcs11PrivKeyYaml)},
+						"pkcs11-config": {p11confYaml},
+					},
+				},
+			},
+
+			DecryptConfig: &config.DecryptConfig{
+				Parameters: map[string][][]byte{
+					"pkcs11-yamls":  {[]byte(pkcs11PrivKeyYaml)},
+					"pkcs11-config": {p11confYaml},
+				},
+			},
+		},
+	}
+	return validPkcs11Ccs, shsm, nil
+}
+
+func createInvalidPkcs11Ccs(t *testing.T) ([]*config.CryptoConfig, *softhsm.SoftHSMSetup, error) {
+	shsm := softhsm.NewSoftHSMSetup()
+	pkcs11PubKeyUriStr, err := shsm.RunSoftHSMSetup(SOFTHSM_SETUP)
+	if err != nil {
+		return nil, shsm, err
+	}
+	pubKey2Pem, _, err := utils.CreateRSATestKey(2048, nil, true)
+	if err != nil {
+		return nil, shsm, err
+	}
+	pkcs11PrivKeyYaml := `
+pkcs11:
+  uri: ` + pkcs11PubKeyUriStr + `
+module:
+  env:
+    SOFTHSM2_CONF: ` + shsm.GetConfigFilename()
+
+	p11confYaml := getPkcs11ConfigYaml(t)
+
+	invalidPkcs11Ccs := []*config.CryptoConfig{
+		// Key 1
+		{
+			EncryptConfig: &config.EncryptConfig{
+				Parameters: map[string][][]byte{
+					"pkcs11-pubkeys": {[]byte(pubKey2Pem)},
+				},
+				DecryptConfig: config.DecryptConfig{
+					Parameters: map[string][][]byte{
+						"pkcs11-yamls":  {[]byte(pkcs11PrivKeyYaml)},
+						"pkcs11-config": {p11confYaml},
+					},
+				},
+			},
+
+			DecryptConfig: &config.DecryptConfig{
+				Parameters: map[string][][]byte{
+					"pkcs11-yamls":  {[]byte(pkcs11PrivKeyYaml)},
+					"pkcs11-config": {p11confYaml},
+				},
+			},
+		},
+	}
+	return invalidPkcs11Ccs, shsm, nil
+}
+
+func TestKeyWrapPkcs11Success(t *testing.T) {
+	validPkcs11Ccs, shsm, err := createValidPkcs11Ccs(t)
+	defer shsm.RunSoftHSMTeardown(SOFTHSM_SETUP)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os.Setenv("OCICRYPT_OAEP_HASHALG", "sha1")
+
+	for _, cc := range validPkcs11Ccs {
+		kw := NewKeyWrapper()
+
+		data := []byte("This is some secret text")
+
+		wk, err := kw.WrapKeys(cc.EncryptConfig, data)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		ud, err := kw.UnwrapKey(cc.DecryptConfig, wk)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if string(data) != string(ud) {
+			t.Fatal("Strings don't match")
+		}
+	}
+}
+
+func TestKeyWrapPkcs11Invalid(t *testing.T) {
+	invalidPkcs11Ccs, shsm, err := createInvalidPkcs11Ccs(t)
+	defer shsm.RunSoftHSMTeardown(SOFTHSM_SETUP)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	os.Setenv("OCICRYPT_OAEP_HASHALG", "sha1")
+
+	for _, cc := range invalidPkcs11Ccs {
+		kw := NewKeyWrapper()
+
+		data := []byte("This is some secret text")
+
+		wk, err := kw.WrapKeys(cc.EncryptConfig, data)
+		if err != nil {
+			t.Fatalf("Wrapping should have worked")
+		}
+
+		ud, err := kw.UnwrapKey(cc.DecryptConfig, wk)
+		if err != nil {
+			continue
+		}
+
+		if string(data) != string(ud) {
+			t.Fatalf("Unwrapping should have failed already")
+		}
+
+		t.Fatal("Successfully wrapped and unwrapped with invalid crypto config")
+	}
+}
